2734fe7e771ca1bb30d563aeff2904c3d582902e
Jalber Dinelli Luna Galindo <87705638+jalberdlg@users.noreply.github.com>
2021-08-03 08:56:38 -0300

    Update PSO-serie.c

--- PSO-serie.c
+++ PSO-serie.c
@@ -1,148 +1,154 @@
 #include <api.h>
 #include <stdlib.h>
 
-/* Função Objetivo */
+/* Definindo as constantes */
+#define w     0.641742430504416      /* Constante da componente inercial */
+#define c1    1.347659104059274    /* Constante da componente social */
+#define c2    1.347659104059274    /* Constante da componente cognitiva */
 
-float fobj (float x1,float x2) {
+Message position, veloc, pbest, gbest;
+/* Função objetivo */
+
+float fobj (float x1, float x2){
     return x1*x1 + x2*x2;
 }
 
 /* PSO */
 
-int main(){
+float main(){
 
     /* Inicialização dos parâmetros */
 
-    int n_var = 2;            /* Número de variáveis da função objetivo */
-    int n_part = 10;        /* Número de partículas */
+    int n_var = 2;
+        int n_part = 10;
+            
+        int iteracoes = 500;
     
-    float w= 0.641742430504416;    /* Constante da componente inercial */
-    float c1 = 1.347659104059274;    /* Constante da componente social */
-    float c2 = 1.347659104059274;    /* Constante da componente cognitiva */
+        int Menor = 0;
+       float MenorFun0, MenorFun;
+       float funcao0, funcao;
     
-    int iteracoes = 500;
-    int qt_opt = 1;
-
-    int Menor = 0;
-    float MenorFun0, MenorFun;
+        int i, j;
     
-    int i, j;
-    
+        Echo("Início do PSO: ");
+        Echo(itoa(GetTick()));
         
-    /* Inicialização das particulas */
-    
-    float position[n_part][n_var];
-    float veloc[n_part][n_var];
-    float pbest[n_part][n_var];
-    float gbest[n_var];
-    float funcao0[n_part], funcao[n_part];
-
-    
-    for(i = 0; i < n_part; i++){
-        for(j = 0; j < n_var; j++){
-            position[i][j] = rand(1,0,100)/100; /* Atribui valores randomicos entre 0 e 1 */
-        }
-    }
-    
-    for(i = 0; i < n_part; i++){
-        funcao0[i] = fobj (position[i][0],position[i][1]); /* Vetor com valores da função objetivo aplicando cada valor inicial das particulas */
-    }
-    
-    for(i = 0; i < n_part; i++){
-        if (funcao0[i] < funcao0[Menor]){
-            Menor = i;
-            MenorFun0 = funcao0[i];
-        }
-    }
-    
-    for(i = 0; i < n_part; i++){
-        for(j = 0; j < n_var; j++){
-            veloc[i][j] = 0.1 * position[i][j];  /* Atribui valores iniciais a veloc */
-            pbest[i][j] = position[i][j];
-        }
-    }
-    for(j = 0; j < n_var; j++){
-            gbest[j] = position[Menor][j];  /* Atribui valores iniciais a veloc */
-    }
+        /* Inicializando os valores das particulas */
         
-    /* Algoritmo PSO */
-    
-    int ite = 0;
-    float target_error = 1.0;
-    float inercial, social, cognitivo = 0.0;
-    
-    while ((ite < iteracoes) && target_error>10^-12){
-        
-        /* Atualização da matriz velocidade */
-        for(i = 0; i < n_part; i++){
-            for(j = 0; j < n_var; j++){
-                inercial = w*veloc[i][j];
-                social = c1*(rand(1,0,100)/100)*(pbest[i][j]-position[i][j]);
-                cognitivo = c2*(rand(1,0,100)/100)*(gbest[j]-position[i][j]);
-                veloc[i][j] = inercial + social + cognitivo;
+        for(i=0; i < MSG_SIZE; i++){ /* MSG_SIZE é definido no api.h como sendo 128 */
+            if (i < n_var*n_part){
+                position.msg[i] = rand(1,0,100)/100; /* Define os valores de position randomicamente entre 0 e 1 */
+            }
+            else{
+                position.msg[i] = 0;
             }
         }
         
-        /* Atualização das particulas */
-        for(i = 0; i < n_part; i++){
-            for(j = 0; j < n_var; j++){
-                position[i][j] = position[i][j] + veloc[i][j];
+        /* Inicializando o vetor funcao0 com os valores das particulas iniciais */
+        
+        for (i=0; i < n_part; i++){
+            funcao0[i] = fobj(position.msg[2*i],position.msg[2*i+1]);
+        }
+        
+        /* Guardando o menor valor da função e a partícula de menor valor */
+        
+        for (i=0; i < n_part; i++){
+            if (funcao0[i] < funcao0[Menor]){
+                Menor = i;
+                MenorFun0 = funcao0[i];
             }
         }
         
-        /* Avaliação da função objetivo para os novos valores das particulas */
-        for(i = 0; i < n_part; i++){
-            funcao[i] = fobj (position[i][0],position[i][1]);
+        /* Inicializando os valores de veloc, pbest e gbest */
+        
+        for (i=0; i < n_var*n_part; i++){
+            veloc.msg[i] = 0.1 * position.msg[i];
+            pbest.msg[i] = position.msg[i];
         }
-    
-        /* Atualizando pbest e o valor da função */
-        for(i = 0; i < n_part; i++){
-            for(j = 0; j < n_var; j++){
+        
+        for (i=0; i < n_var; i++){
+            gbest.msg[i] = position.msg[2*Menor+i];
+        }
+        
+        /* Algoritmo do PSO */
+        
+        int ite = 0;
+        float target_error = 1.0;
+        float inercial = 0.0;
+        float social = 0.0;
+        float cognitivo = 0.0;
+        
+        while ((ite < iteracoes) && (target_error >10^-12)){
+        
+            /* Atualizando o vetor veloc */
+            for (i=0; i < n_var*n_part; i++){
+                inercial = w * veloc.msg[i];
+                social = c1 * (rand(1,0,100)/100) * (pbest.msg[i] - position.msg[i]);
+                if (i % 2 == 0){
+                    cognitivo = c2 * (rand(1,0,100)/100) * (gbest.msg[0] - position.msg[i]);
+                }
+                else{
+                    cognitivo = c2 * (rand(1,0,100)/100) * (gbest.msg[1] - position.msg[i]);
+                }
+                veloc.msg[i] = inercial + social + cognitivo;
+            }
+            
+            /* Atualizando as particulas */
+            for (i=0; i < n_var*n_part; i++){
+                position.msg[i] = position.msg[i] + veloc.msg[i];
+            }
+            
+            /* Avaliação da função objetivo para os novos valores das particulas */
+            for (i=0; i < n_part; i++){
+                funcao[i] = fobj(position.msg[2*i],position.msg[2*i+1]);
+            }
+            
+            for (i=0; i < n_part; i++){
                 if (funcao[i] < funcao0[i]){
-                    pbest[i][j] = position[i][j];
+                    for (j=0; j < n_part; j++){
+                        pbest.msg[2*i+j] = position.msg[2*i+j];
+                    }
                     funcao0[i] = funcao[i];
                 }
             }
-        }
-        
-        /* Atualizando função */
-        for(i = 0; i < n_part; i++){
-            if (funcao[i] < funcao[Menor]){
-                Menor = i;
-                MenorFun = funcao[i];
-               }
-        }
-        
-        /* Cálculo da tolerancia */
-        target_error = MenorFun - MenorFun0;
-        if (target_error < 0){
-            target_error = - target_error;
-        }
-
-        /* Atualizando gbest e o melhor valor da função */
-        for(j = 0; j < n_var; j++){
-            if (MenorFun < MenorFun0){
-                gbest[j] = pbest[Menor][j];
-                MenorFun0 = MenorFun;
+            
+            /* Atualizando o menor valor da função e a melhor particula */
+            for (i=0; i < n_part; i++){
+                if (funcao[i] < funcao[Menor]){
+                    Menor = i;
+                     MenorFun = funcao[i];
+                }
             }
+            
+            /* Cálculo da tolerância */
+            target_error = MenorFun - MenorFun0;
+            if (target_error < 0){
+                target_error = - target_error;
+            }
+            
+            /* Atualizando gbest e o melhor valor da função */
+            for (i=0; i < n_var; i++){
+                if (MenorFun < MenorFun0){
+                    gbest.msg[i] = pbest.msg[2*Menor+i];
+                    MenorFun0 = MenorFun;
+                }
+            }
+            
+            /* Imprimindo os resultados */
+            Echo("Iteracao: ");
+            Echo(itoa(ite));
+            Echo("Melhor Particula: ");
+            Echo(itoa(Menor));
+            Echo("Valor da função objetivo: ");
+            Echo(itoa(MenorFun));
+            ite = ite + 1;
         }
         
-        /* Imprimindo resultados */
-        Echo("Iteracao: ");
-        Echo(itoa(ite));
-        Echo("Melhor Particula: ");
-        Echo(itoa(Menor));
-        Echo("Função Objetivo: ");
+        Echo("Melhor resultado da função: ");
         Echo(itoa(MenorFun));
-        ite = ite + 1;
-    }
-    
-    Echo("Melhor resultado da funcao: ");
-    Echo(itoa(MenorFun));
-    Echo("Melhor solucao: ");
-    Echo("x1 = ");
-    Echo(itoa(gbest[0]));
-    Echo("x2 = ");
-    Echo(itoa(gbest[1]));
-    
+        Echo("Melhor solução: ");
+        Echo("X1 = ");
+        Echo(itoa(gbest.msg[0]));
+        Echo("X2 = ");
+        Echo(itoa(gbest.msg[1]));     
 }
